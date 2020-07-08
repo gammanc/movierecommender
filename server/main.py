477a80f30d4911c850479d4f25c225c14ccbb8d6
Kevin Velasquez <kevin.vela519@gmail.com>
2020-07-08 12:32:11 -0600

    Added endpoint for popular movies

--- server/main.py
+++ server/main.py
@@ -1,14 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from data_utils import retrieve_popular_movies
 
 app = Flask(__name__)
 app.config['ENV'] = "development"
 
 CORS(app)
 
-@app.route("/")
-def home():
-    return jsonify({ "hello": "world"})
+@app.route("/api/movies/popular")
+def retrieve_popular():
+    list_size = int(request.args.get("max", "10"))
+    list_size = 5 if list_size < 5 else list_size
+    return retrieve_popular_movies(list_size)
 
 if __name__ == "__main__":
     app.run(debug=True)
